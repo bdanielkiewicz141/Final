53fb7f42de27887f5f29b0d8cfd2ff406676439d
bdani <b.danielkiewicz141@gmail.com>
2021-01-10 17:11:26 +0100

    Order develop

--- src/main/java/pl/sda/zdjavapol4/moviesrentalwebapp/models/Order.java
+++ src/main/java/pl/sda/zdjavapol4/moviesrentalwebapp/models/Order.java
@@ -1,8 +1,30 @@
 package pl.sda.zdjavapol4.moviesrentalwebapp.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ORDER")
 public class Order {
+
+    @Id
+    @GeneratedValue
     private Long copyID;
+
+
     private int movieID;
+
     private String courier;
     private String orderStatus;
     private int clientID;
