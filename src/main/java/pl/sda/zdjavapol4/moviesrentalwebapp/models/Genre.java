b99a2e15fccbd13561e3b43af2b506614795e0dc
Kshmr <cwicze@gita.pl>
2021-01-10 17:34:56 +0100

    edit enums

--- src/main/java/pl/sda/zdjavapol4/moviesrentalwebapp/models/Genre.java
+++ src/main/java/pl/sda/zdjavapol4/moviesrentalwebapp/models/Genre.java
@@ -1,15 +1,15 @@
 package pl.sda.zdjavapol4.moviesrentalwebapp.models;
 
 public enum Genre {
-    Documentary,
-    Thriller,
-    Musical,
-    Comedy,
-    Horror,
-    Sci_Fi,
-    Action,
-    Drama,
-    Romance,
-    Historical
+    DOCUMENTARY,
+    THRILLER,
+    MUSICAL,
+    COMEDY,
+    HORROR,
+    SCI_FI,
+    ACTION,
+    DRAMA,
+    ROMANCE,
+    HISTORICAL
 
 }
