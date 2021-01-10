b9b66d2dfbffbfbc0d5f6423c789ff084652d8cd
bdani <b.danielkiewicz141@gmail.com>
2021-01-10 17:53:56 +0100

    Client addnotation created

--- src/main/java/pl/sda/zdjavapol4/moviesrentalwebapp/models/Client.java
+++ src/main/java/pl/sda/zdjavapol4/moviesrentalwebapp/models/Client.java
@@ -1,13 +1,85 @@
 package pl.sda.zdjavapol4.moviesrentalwebapp.models;
 
-import org.hibernate.annotations.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+import javax.persistence.*;
 
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "CLIENT")
 public class Client {
-    private int clientID;
-    private String login;
-    private String email;
-    private String adress;
-    private String fullName;
-    private long cardNumber;
+
+     @Id
+     @GeneratedValue
+     int clientID;
+     @Column(nullable = false)
+     String login;
+     @Column(nullable = false)
+     String Password;
+     String email;
+     String adress;
+     String fullName;
+     long cardNumber;
+
+     public int getClientID() {
+          return clientID;
+     }
+
+     public void setClientID(int clientID) {
+          this.clientID = clientID;
+     }
+
+     public String getLogin() {
+          return login;
+     }
+
+     public void setLogin(String login) {
+          this.login = login;
+     }
+
+     public String getPassword() {
+          return Password;
+     }
+
+     public void setPassword(String password) {
+          Password = password;
+     }
+
+     public String getEmail() {
+          return email;
+     }
+
+     public void setEmail(String email) {
+          this.email = email;
+     }
+
+     public String getAdress() {
+          return adress;
+     }
+
+     public void setAdress(String adress) {
+          this.adress = adress;
+     }
+
+     public String getFullName() {
+          return fullName;
+     }
+
+     public void setFullName(String fullName) {
+          this.fullName = fullName;
+     }
+
+     public long getCardNumber() {
+          return cardNumber;
+     }
+
+     public void setCardNumber(long cardNumber) {
+          this.cardNumber = cardNumber;
+     }
 }
