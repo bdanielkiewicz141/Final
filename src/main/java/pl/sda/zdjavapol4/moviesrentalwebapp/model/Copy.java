e1295da25929f8a296bf1f0d91420edb3b745d26
Przemyslaw Wozniak <inz.przemyslaw.wozniak@gmail.com>
2021-01-16 11:52:52 +0100

    Pomoc trenera

--- src/main/java/pl/sda/zdjavapol4/moviesrentalwebapp/model/Copy.java
+++ src/main/java/pl/sda/zdjavapol4/moviesrentalwebapp/model/Copy.java
@@ -1,58 +1,24 @@
-package pl.sda.zdjavapol4.moviesrentalwebapp.model;
+package pl.sda.zdjavapol4.moviesrentalwebapp.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.*;
 
-@Builder
-@ToString
+@Getter
+@Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
-@Table(name = "COPIES")
-
 public class Copy {
-
 
     @Id
     @GeneratedValue
-    Long copyId;
-
-    public Copy(Long copyId) {
-        this.copyId = copyId;
-    }
+    Long id;
 
     @ManyToOne
-    Movie movieId;
+    Movie movie;
 
     @Enumerated(EnumType.ORDINAL)
     CopyStatus copyStatus;
 
-    public long getCopyId() {
-        return copyId;
-    }
-
-    public void setCopyId(long copyID) {
-        this.copyId = copyId;
-    }
-
-    public Movie getMovieId() {
-        return movieId;
-    }
-
-    public void setMovieId(Movie movieId) {
-        this.movieId = movieId;
-    }
-
-    public CopyStatus getCopyStatus() {
-        return copyStatus;
-    }
-
-    public void setCopyStatus(CopyStatus copyStatus) {
-        this.copyStatus = copyStatus;
-    }
 }
 
