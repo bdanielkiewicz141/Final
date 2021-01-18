97f45ebf0c8b1f3fc4c4abeab3b9f2e140e44e6b
bdani <b.danielkiewicz141@gmail.com>
2021-01-18 22:59:33 +0100

    Contoller create

--- src/main/java/pl/sda/zdjavapol4/moviesrentalwebapp/controller/MovieController.java
+++ src/main/java/pl/sda/zdjavapol4/moviesrentalwebapp/controller/MovieController.java
@@ -2,7 +2,9 @@ package pl.sda.zdjavapol4.moviesrentalwebapp.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import pl.sda.zdjavapol4.moviesrentalwebapp.exceptions.MovieAlreadyExistsException;
 import pl.sda.zdjavapol4.moviesrentalwebapp.models.Movie;
@@ -16,7 +18,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MovieController {
 
-    private final MovieService movieService;
+     private MovieService movieService;
+
 
     @GetMapping("/movie-list")
     public ResponseEntity<List<Movie>> getAllMovies() {
@@ -26,7 +29,7 @@ public class MovieController {
                 .body(movieService.findAll());
     }
 
-    //    @CrossOrigin(origins = "http://localhost:3000")
+    //    @CrossOrigin(origins = "http://localhost:3000/%22)
     @PostMapping("/movie-list")
     public ResponseEntity<?> createMovie(@RequestBody final Movie movie) throws MovieAlreadyExistsException {
         log.info("New movie has been created");
@@ -37,4 +40,3 @@ public class MovieController {
     }
 
 }
-
